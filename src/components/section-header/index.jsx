06678faa0d9f29c0342e06dcb0b34a8d4dec86ff
TreacherousZ <zhangheran@zhangherandeMacBook-Pro-2.local>
2024-09-07 16:25:12 +0800

    app-footer, section栏目, 每个房间的小组件搭建

--- src/components/section-header/index.jsx
+++ src/components/section-header/index.jsx
@@ -3,12 +3,12 @@ import React, { memo } from 'react'
 import { HeaderWrapper } from './style'
 
 const SectionHeader = memo((props) => {
-  const { title, subtitle  } = props
+  const { title, subtitle } = props
 
   return (
     <HeaderWrapper>
       <h2 className='title'>{title}</h2>
-      { subtitle && <div className='subtitle'>{subtitle}</div>}
+      { subtitle && <div className='subtitle'>{subtitle}</div> }
     </HeaderWrapper>
   )
 })
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,44 +1,35 @@
-import React, { memo, useEffect } from 'react';
-import { HomeWrapper } from './style';
-import HomeBanner from './c-cpns/home-banner';
-import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { fetchHomeDataAction } from '@/store/modules/home';
-import SectionHeader from '@/components/selection-header';
+import React, { memo, useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+
+import { fetchHomeDataAction } from '@/store/modules/home'
+import HomeBanner from './c-cpns/home-banner'
+import { HomeWrapper } from './style'
+import SectionHeader from '@/components/section-header'
+import SectionRooms from '@/components/section-rooms'
 
 const Home = memo(() => {
-  const { goodPriceInfo } = useSelector(
-    (state) => ({
-      goodPriceInfo: state.home.goodPriceInfo,
-    }),
-    shallowEqual
-  );
+  /** 从redux中获取数据 */
+  const { goodPriceInfo } = useSelector((state) => ({
+    goodPriceInfo: state.home.goodPriceInfo
+  }), shallowEqual)
 
-  const dispatch = useDispatch();
-
+  /** 派发异步的事件: 发送网络请求 */
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchHomeDataAction('xxx'));
-  }, [dispatch]);
+    dispatch(fetchHomeDataAction("xxxx"))
+  }, [dispatch])
 
   return (
     <HomeWrapper>
-      <HomeBanner />
-      <div className="content">
+      <HomeBanner/>
+      <div className='content'>
         <div className='good-price'>
-          <SectionHeader title={goodPriceInfo.title} />
-          {/* 确保 goodPriceInfo 和 list 已经定义 */}
-          <ul>
-            {goodPriceInfo?.list?.length > 0 ? (
-              goodPriceInfo.list.map((item) => (
-                <li key={item.id}>{item.name}</li>
-              ))
-            ) : (
-              <li>No items available</li>
-            )}
-          </ul>
+          <SectionHeader title={goodPriceInfo.title}/>
+          <SectionRooms roomList={goodPriceInfo.list}/>
         </div>
       </div>
     </HomeWrapper>
-  );
-});
+  )
+})
 
-export default Home;
+export default Home
